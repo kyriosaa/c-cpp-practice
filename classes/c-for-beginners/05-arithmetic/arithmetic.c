6cc9e3b560d2512445b3f820d0527dfa0bfab290
kyriosaa <kyriosaaph@gmail.com>
2025-01-31 15:49:22 +0700

    05-arithmetic

---
+++ classes/c-for-beginners/05-arithmetic/arithmetic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    /*
+
+     + (addition)
+     - (subtraction)
+     * (multiplication)
+     / (division)
+     % (modulus)
+     ++ (increment by 1)
+     -- (decrement by 1)
+
+    */
+
+   int x = 8;
+   int y = 5;
+
+   float z = x / (float) y; // because division is weird
+
+   printf("%f", z);
+
+   return 0;
+}
